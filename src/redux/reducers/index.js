ccf1336bc31820670aef015a37276f9152b16e5b
Sakkthi <sakkthigurru@gmail.com>
2025-01-20 08:28:44 +0000

    user-directory_Assignment

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
+import {combineReducers} from 'redux'
+import userReducer from './userReducer'
 
 const rootReducer = combineReducers({
   users: userReducer,
   // Add other reducers if needed
-});
+})
 
-export default rootReducer;
+export default rootReducer
