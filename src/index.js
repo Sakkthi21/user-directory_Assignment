514dc3dc675ccb306cc8abd4b2614664713febdc
Sakkthi21 <158794277+Sakkthi21@users.noreply.github.com>
2025-01-20 17:00:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from react-dom/client
+import ReactDOM from 'react-dom/client'; 
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); // Create a root
+const root = ReactDOM.createRoot(container); 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
